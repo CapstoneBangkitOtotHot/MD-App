ccf5240d8ba633a06ab0237654d93f258b818374
Chandra <cwirabuana@gmail.com>
2024-05-22 20:11:36 +0700

    push

--- app/src/main/java/com/cwb/freshmeter/MainActivity.kt
+++ app/src/main/java/com/cwb/freshmeter/MainActivity.kt
@@ -8,18 +8,26 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     private lateinit var moveToCameraButton: Button
+    private lateinit var moveToProfileButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         moveToCameraButton = findViewById(R.id.camera)
+        moveToProfileButton = findViewById(R.id.ProfileButton)
 
         moveToCameraButton.setOnClickListener { onClickMoveToCamera() }
+        moveToProfileButton.setOnClickListener { onClickMoveToProfile() }
     }
 
     private fun onClickMoveToCamera() {
         val intent = Intent(this, CameraActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onClickMoveToProfile() {
+        val intent = Intent(this, ProfileActivity::class.java)
         startActivity(intent)
     }
 }
